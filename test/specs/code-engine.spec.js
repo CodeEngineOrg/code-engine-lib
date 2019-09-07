aad95f3588c655b7e8e39481477b53d46898f6b4
James Messinger <james.messinger@gmail.com>
2019-09-07 05:03:36 -0500

    Added tests for the CodeEngine.toString() functionality

--- test/specs/code-engine.spec.js
+++ test/specs/code-engine.spec.js
@@ -10,7 +10,7 @@ describe("CodeEngine class", () => {
 
     try {
       engine = new CodeEngine();
-      expect(engine).to.be.an("object");
+      expect(engine).to.be.a("CodeEngine");
     }
     finally {
       engine && await engine.dispose();
@@ -22,7 +22,7 @@ describe("CodeEngine class", () => {
 
     try {
       engine = new CodeEngine({});
-      expect(engine).to.be.an("object");
+      expect(engine).to.be.a("CodeEngine");
     }
     finally {
       engine && await engine.dispose();
@@ -34,7 +34,7 @@ describe("CodeEngine class", () => {
 
     try {
       engine = new CodeEngine({ foo: true, bar: 5 });
-      expect(engine).to.be.an("object");
+      expect(engine).to.be.a("CodeEngine");
     }
     finally {
       engine && await engine.dispose();
@@ -49,6 +49,29 @@ describe("CodeEngine class", () => {
 
     expect(withoutNew).to.throw(TypeError);
     expect(withoutNew).to.throw("Class constructor CodeEngine cannot be invoked without 'new'");
+  });
+
+  it("should support toString()", async () => {
+    let engine;
+
+    try {
+      engine = new CodeEngine();
+      expect(Object.prototype.toString.call(engine)).to.equal("[object CodeEngine]");
+      expect(engine.toString()).to.equal("CodeEngine (0 plugins)");
+
+      await engine.use({ name: "Plugin 1" });
+      expect(Object.prototype.toString.call(engine)).to.equal("[object CodeEngine]");
+      expect(engine.toString()).to.equal("CodeEngine (1 plugins)");
+
+      await engine.use({ name: "Plugin 2" }, { name: "Plugin 3" });
+      expect(Object.prototype.toString.call(engine)).to.equal("[object CodeEngine]");
+      expect(engine.toString()).to.equal("CodeEngine (3 plugins)");
+    }
+    finally {
+      engine && await engine.dispose();
+      expect(Object.prototype.toString.call(engine)).to.equal("[object CodeEngine]");
+      expect(engine.toString()).to.equal("CodeEngine (disposed)");
+    }
   });
 
 });
