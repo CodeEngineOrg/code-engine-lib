5d30d7b788da3f6f815533eafc1fa3824da8b5e4
James Messinger <james.messinger@gmail.com>
2019-09-10 19:15:16 -0500

    Don't even include an `error` property if there isn't one

--- src/loggers/split-error.ts
+++ src/loggers/split-error.ts
@@ -4,19 +4,17 @@ import { env } from "../env";
  * Splits an Error or error message into two separate values.
  */
 export function splitError(arg: string | Error) {
-  let message: string, error: Error | undefined;
-
   if (typeof arg === "string") {
-    message = arg;
+    return { message: arg };
   }
   else {
-    error = arg;
-    message = arg.message || String(arg);
+    let error = arg;
+    let message = arg.message || String(arg);
 
     if (env.isDebug) {
       message = arg.stack || message;
     }
-  }
 
-  return { message, error };
+    return { message, error };
+  }
 }
