0c93e0ea4938d2098db7d122d44c2cec6e2a6d1f
James Messinger <james.messinger@gmail.com>
2019-09-14 15:54:26 -0500

    Refactored the `Plugin.processFile()` tests so that each test is run on the main thread and on a worker thread to ensure consistent behavior

--- test/specs/process-file.spec.js
+++ test/specs/process-file.spec.js
@@ -1,306 +1,229 @@
 "use strict";
 
-const sinon = require("sinon");
 const CodeEngine = require("../utils/code-engine");
-const { createModule, delay } = require("../utils/utils");
+const { testThreadConsistency } = require("../utils/utils");
+const { expect } = require("chai");
+const sinon = require("sinon");
 
 describe("Plugin.processFile()", () => {
+  testThreadConsistency((createPlugin) => {
 
-  it("should do nothing if no plugins implement processFile", async () => {
-    let plugin1 = {
-      name: "Plugin 1",
-      find: sinon.stub().returns([]),
-    };
-    let plugin2 = {
-      name: "Plugin 2",
-      find: sinon.stub().returns([]),
-    };
+    it("should do nothing if no plugins implement processFile", async () => {
+      let plugin1 = {
+        name: "Plugin 1",
+        find: sinon.stub().returns([]),
+      };
+      let plugin2 = await createPlugin(() => ({
+        name: "Plugin 2",
+      }));
 
-    let engine = CodeEngine.create();
-    await engine.use(plugin1, plugin2);
-    await engine.build();
+      let engine = CodeEngine.create();
+      await engine.use(plugin1, plugin2);
+      await engine.build();
 
-    sinon.assert.calledOnce(plugin1.find);
-    sinon.assert.calledOnce(plugin2.find);
-  });
+      sinon.assert.calledOnce(plugin1.find);
+    });
 
-  it("should call the processFile() method for each file", async () => {
-    let plugin = {
-      name: "Plugin",
-      *find () {
-        yield { path: "file1.txt" };
-        yield { path: "file2.txt" };
-        yield { path: "file3.txt" };
-      },
-      processFile: sinon.spy(),
-    };
+    it("should call the processFile() method for each file", async () => {
+      let plugin1 = {
+        name: "Plugin 1",
+        *find () {
+          yield { path: "file1.txt" };
+          yield { path: "file2.txt" };
+          yield { path: "file3.txt" };
+        },
+        processFile: sinon.spy(),
+      };
 
-    let engine = CodeEngine.create();
-    await engine.use(plugin);
-    await engine.build();
+      let plugin2 = await createPlugin(() => ({
+        name: "Plugin 2",
+        processFile (file) {
+          file.contents = Buffer.from("Plugin 2 was here");
+        }
+      }));
 
-    sinon.assert.calledThrice(plugin.processFile);
-  });
+      let engine = CodeEngine.create();
+      await engine.use(plugin1, plugin2);
+      let files = await engine.build();
 
-  it("should call the processFile() method of all plugins", async () => {
-    let plugin1 = {
-      name: "Plugin 1",
-      *find () {
-        yield { path: "file1.txt" };
-        yield { path: "file2.txt" };
-        yield { path: "file3.txt" };
-        yield { path: "file4.txt" };
-      },
-      processFile: sinon.spy(),
-    };
-    let plugin2 = {
-      name: "Plugin 2",
-      processFile: sinon.spy(),
-    };
-    let plugin3 = {
-      name: "Plugin 3",
-      processFile: sinon.spy(),
-    };
+      sinon.assert.calledThrice(plugin1.processFile);
+      expect(files.size).to.equal(3);
+      for (let file of files) {
+        expect(file.contents.toString()).to.equal("Plugin 2 was here");
+      }
+    });
 
-    let engine = CodeEngine.create();
-    await engine.use(plugin1, plugin2, plugin3);
-    await engine.build();
+    it("should call the processFile() method of all plugins", async () => {
+      let plugin1 = {
+        name: "Plugin 1",
+        *find () {
+          yield { path: "file1.txt" };
+          yield { path: "file2.txt" };
+          yield { path: "file3.txt" };
+          yield { path: "file4.txt" };
+        },
+        processFile: sinon.spy(),
+      };
+      let plugin2 = {
+        name: "Plugin 2",
+        processFile: sinon.spy(),
+      };
+      let plugin3 = {
+        name: "Plugin 3",
+        processFile: sinon.spy(),
+      };
+      let plugin4 = await createPlugin(() => ({
+        name: "Plugin 4",
+        processFile (file) {
+          file.contents = Buffer.from("Plugin 4 was here");
+        }
+      }));
 
-    sinon.assert.callCount(plugin1.processFile, 4);
-    sinon.assert.callCount(plugin2.processFile, 4);
-    sinon.assert.callCount(plugin3.processFile, 4);
-  });
+      let engine = CodeEngine.create();
+      await engine.use(plugin1, plugin2, plugin3, plugin4);
+      let files = await engine.build();
 
-  it("should call the processFile() methods in order for each file", async () => {
-    let source = {
-      name: "File Source",
-      *find () {
-        yield { path: "file1.txt" };
-        yield { path: "file2.txt" };
-        yield { path: "file3.txt" };
-      },
-    };
-    let processor1 = {
-      name: "File Processor 1",
-      processFile: sinon.spy(),
-    };
-    let processor2 = {
-      name: "File Processor 2",
-      processFile: sinon.spy(),
-    };
-    let processor3 = {
-      name: "File Processor 3",
-      processFile: sinon.spy(),
-    };
+      sinon.assert.callCount(plugin1.processFile, 4);
+      sinon.assert.callCount(plugin2.processFile, 4);
+      sinon.assert.callCount(plugin3.processFile, 4);
 
-    let engine = CodeEngine.create();
-    await engine.use(source, processor1, processor2, processor3);
-    await engine.build();
+      expect(files.size).to.equal(4);
+      for (let file of files) {
+        expect(file.contents.toString()).to.equal("Plugin 4 was here");
+      }
+    });
 
-    sinon.assert.calledThrice(processor1.processFile);
-    sinon.assert.calledThrice(processor2.processFile);
-    sinon.assert.calledThrice(processor3.processFile);
+    it("should call the processFile() methods in order for each file", async () => {
+      let source = {
+        name: "File Source",
+        *find () {
+          yield { path: "file1.txt" };
+          yield { path: "file2.txt" };
+          yield { path: "file3.txt" };
+        },
+      };
+      let processor1 = await createPlugin(() => ({
+        name: "File Processor 1",
+        processFile (file) {
+          file.contents = Buffer.from(String(file.contents) + "1");
+        },
+      }));
+      let processor2 = await createPlugin(() => ({
+        name: "File Processor 2",
+        processFile (file) {
+          file.contents = Buffer.from(String(file.contents) + "2");
+        },
+      }));
+      let processor3 = await createPlugin(() => ({
+        name: "File Processor 3",
+        processFile (file) {
+          file.contents = Buffer.from(String(file.contents) + "3");
+        },
+      }));
 
-    assertCallOrder(
-      processor1.processFile.getCalls().find((call) => call.args[0].path === "file1.txt"),
-      processor2.processFile.getCalls().find((call) => call.args[0].path === "file1.txt"),
-      processor3.processFile.getCalls().find((call) => call.args[0].path === "file1.txt"),
-    );
+      let engine = CodeEngine.create();
+      await engine.use(source, processor1, processor2, processor3);
+      let files = await engine.build();
 
-    assertCallOrder(
-      processor1.processFile.getCalls().find((call) => call.args[0].path === "file2.txt"),
-      processor2.processFile.getCalls().find((call) => call.args[0].path === "file2.txt"),
-      processor3.processFile.getCalls().find((call) => call.args[0].path === "file2.txt"),
-    );
+      expect(files.size).to.equal(3);
+      for (let file of files) {
+        expect(file.contents.toString()).to.equal("123");
+      }
+    });
 
-    assertCallOrder(
-      processor1.processFile.getCalls().find((call) => call.args[0].path === "file3.txt"),
-      processor2.processFile.getCalls().find((call) => call.args[0].path === "file3.txt"),
-      processor3.processFile.getCalls().find((call) => call.args[0].path === "file3.txt"),
-    );
-  });
+    it("should move each file through the plugin pipeline separately", async () => {
+      let source = {
+        name: "File Source",
+        *find () {
+          yield { path: "file1.txt", contents: "[]" };
+          yield { path: "file2.txt", contents: "[]" };
+          yield { path: "file3.txt", contents: "[]" };
+        },
+      };
 
-  it("should move each file through the plugin pipeline separately", async () => {
-    /* eslint-disable camelcase */
-    let source = {
-      name: "File Source",
-      *find () {
-        yield { path: "file1.txt", metadata: { fileId: 1 }};
-        yield { path: "file2.txt", metadata: { fileId: 2 }};
-        yield { path: "file3.txt", metadata: { fileId: 3 }};
-      },
-    };
-    let processor1 = {
-      name: "File Processor 1",
-      processFile: sinon.spy(function processor1_procesFile ({ metadata: { fileId }}) {
-        return delay(fileId === 1 ? 500 : fileId === 2 ? 100 : 10);
-      })
-    };
-    let processor2 = {
-      name: "File Processor 2",
-      processFile: sinon.spy(function processor2_processFile ({ metadata: { fileId }}) {
-        return delay(fileId === 1 ? 10 : fileId === 2 ? 500 : 10);
-      })
-    };
-    let processor3 = {
-      name: "File Processor 3",
-      processFile: sinon.spy(function processor3_processFile () {
-        return delay(10);
-      })
-    };
-
-    let engine = CodeEngine.create();
-    await engine.use(source, processor1, processor2, processor3);
-    await engine.build();
-
-    sinon.assert.calledThrice(processor1.processFile);
-    sinon.assert.calledThrice(processor2.processFile);
-    sinon.assert.calledThrice(processor3.processFile);
-
-    assertCallOrder(
-      // All of the processor1 calls should happen before ANY of the processor2 calls
-      processor1.processFile.getCall(0),
-      processor1.processFile.getCall(1),
-      processor1.processFile.getCall(2),
-
-      // file3 should be fully processed before file1 or file2 finishes processor1
-      processor2.processFile.getCalls().find((call) => call.args[0].path === "file3.txt"),
-      processor3.processFile.getCalls().find((call) => call.args[0].path === "file3.txt"),
-
-      // // Next, file2 finishes processor1 and starts processor2
-      processor2.processFile.getCalls().find((call) => call.args[0].path === "file2.txt"),
-
-      // // Next, file1 finishes processor1 and is processed by processor2 and processor3
-      processor2.processFile.getCalls().find((call) => call.args[0].path === "file1.txt"),
-      processor3.processFile.getCalls().find((call) => call.args[0].path === "file1.txt"),
-
-      // // And finally, file2 finishes processor2 and starts processor3
-      processor3.processFile.getCalls().find((call) => call.args[0].path === "file2.txt"),
-    );
-  });
-
-  it("should move each file through the plugin pipeline separately (on worker threads)", async () => {
-    let source = {
-      name: "File Source",
-      *find () {
-        yield { path: "file1.txt", contents: "[]" };
-        yield { path: "file2.txt", contents: "[]" };
-        yield { path: "file3.txt", contents: "[]" };
-      },
-    };
-
-    let pluginCode = `
-      module.exports = ({ processorId, delays }) => {
+      function pluginFactory ({ processorId, delays }) {
         return {
           name: "File Processor " + processorId,
-          async processFile (file, context) {
-            // Mimic a Sinon spy call
-            let call = {
-              callId: Date.now(),
-              args: [file.toString()],
-              proxy: { displayName: "processFile" },
-            };
-
+          async processFile (file) {
+            let now = new Date();
             let delay = delays[file.path];
             await new Promise((resolve) => setTimeout(resolve, delay));
 
-            context.logger.log("This is a log");
-            context.logger.warn("This is a warning", { foo: "bar", now: new Date("2018-05-15T19:35:45.123Z") })
-
             let array = JSON.parse(String(file.contents));
-            array.push({ processorId, ...call });
+            array.push({ path: file.path, processorId, now });
             file.contents = Buffer.from(JSON.stringify(array));
           }
         };
-      };
-    `;
+      }
 
-    let processor1 = {
-      moduleId: await createModule(pluginCode),
-      data: {
+      let processor1 = await createPlugin(pluginFactory, {
         processorId: 1,
         delays: {
           "file1.txt": 500,
           "file2.txt": 100,
           "file3.txt": 10,
         },
-      }
-    };
-    let processor2 = {
-      moduleId: await createModule(pluginCode),
-      data: {
+      });
+      let processor2 = await createPlugin(pluginFactory, {
         processorId: 2,
         delays: {
           "file1.txt": 10,
           "file2.txt": 500,
           "file3.txt": 10,
         },
-      }
-    };
-    let processor3 = {
-      moduleId: await createModule(pluginCode),
-      data: {
+      });
+      let processor3 = await createPlugin(pluginFactory, {
         processorId: 3,
         delays: {
           "file1.txt": 10,
           "file2.txt": 10,
           "file3.txt": 10,
         },
+      });
+
+      let engine = CodeEngine.create({ concurrency: 1 });
+      await engine.use(source, processor1, processor2, processor3);
+      let files = await engine.build();
+
+      let calls = [
+        ...JSON.parse(files.demand("file1.txt").contents.toString()),
+        ...JSON.parse(files.demand("file2.txt").contents.toString()),
+        ...JSON.parse(files.demand("file3.txt").contents.toString()),
+      ];
+
+      calls.sort((a, b) => new Date(a.now) - new Date(b.now));
+
+      let expectedOrder = [
+        // All of the processor1 calls should happen before ANY of the processor2 calls
+        calls.find(({ path, processorId }) => path === "file1.txt" && processorId === 1),
+        calls.find(({ path, processorId }) => path === "file2.txt" && processorId === 1),
+        calls.find(({ path, processorId }) => path === "file3.txt" && processorId === 1),
+
+        // file3 should be fully processed before file1 or file2 finishes processor1
+        calls.find(({ path, processorId }) => path === "file3.txt" && processorId === 2),
+        calls.find(({ path, processorId }) => path === "file3.txt" && processorId === 3),
+
+        // // Next, file2 finishes processor1 and starts processor2
+        calls.find(({ path, processorId }) => path === "file2.txt" && processorId === 2),
+
+        // // Next, file1 finishes processor1 and is processed by processor2 and processor3
+        calls.find(({ path, processorId }) => path === "file1.txt" && processorId === 2),
+        calls.find(({ path, processorId }) => path === "file1.txt" && processorId === 3),
+
+        // // And finally, file2 finishes processor2 and starts processor3
+        calls.find(({ path, processorId }) => path === "file2.txt" && processorId === 3),
+      ];
+
+      try {
+        expect(calls).to.deep.equal(expectedOrder);
       }
-    };
+      catch (e) {
+        throw new Error(
+          "Incorrect call order.  Actual order was:\n  " +
+          calls.map(({ path, processorId, now }) => `${now} processor${processorId}(${path})`).join("\n  ")
+        );
+      }
+    });
 
-    let engine = CodeEngine.create({ concurrency: 1 });
-    await engine.use(source, processor1, processor2, processor3);
-    let files = await engine.build();
-
-    let file1Calls = JSON.parse(files.demand("file1.txt").contents.toString());
-    let file2Calls = JSON.parse(files.demand("file2.txt").contents.toString());
-    let file3Calls = JSON.parse(files.demand("file3.txt").contents.toString());
-
-    assertCallOrder(
-      // All of the processor1 calls should happen before ANY of the processor2 calls
-      file1Calls.find((call) => call.processorId === 1),
-      file2Calls.find((call) => call.processorId === 1),
-      file3Calls.find((call) => call.processorId === 1),
-
-      // file3 should be fully processed before file1 or file2 finishes processor1
-      file3Calls.find((call) => call.processorId === 2),
-      file3Calls.find((call) => call.processorId === 3),
-
-      // // Next, file2 finishes processor1 and starts processor2
-      file2Calls.find((call) => call.processorId === 2),
-
-      // // Next, file1 finishes processor1 and is processed by processor2 and processor3
-      file1Calls.find((call) => call.processorId === 2),
-      file1Calls.find((call) => call.processorId === 3),
-
-      // // And finally, file2 finishes processor2 and starts processor3
-      file2Calls.find((call) => call.processorId === 3),
-    );
   });
-
 });
-
-
-/**
- * Asserts the given Sinon calls were called in the given order.
- *
- * @param calls {...SinonSpyCall} - The Sinon calls to check the order of
- */
-function assertCallOrder (...calls) {
-  let actualOrder = calls.slice().sort((a, b) => {
-    return a.callId - b.callId;
-  });
-
-  for (let i = 1; i < calls.length; i++) {
-    let expected = calls[i];
-    let actual = actualOrder[i];
-
-    if (actual !== expected) {
-      throw new Error(
-        "Incorrect call order. Actual order was:\n  " +
-        actualOrder.map((call) => `${call.proxy.displayName}(${call.args[0]})`).join("\n  ")
-      );
-    }
-  }
-}
