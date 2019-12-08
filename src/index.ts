660b4a7033023b5c96282edf96df0b670dc80e6d
James Messinger <james.messinger@gmail.com>
2019-12-08 13:50:43 -0600

    Only export as ESM, not CommonJS

--- src/index.ts
+++ src/index.ts
@@ -7,8 +7,3 @@ export { CodeEngine };
 // Export `CodeEngine` as the default export
 // tslint:disable: no-default-export
 export default CodeEngine;
-
-// CommonJS default export hack
-if (typeof module === "object" && typeof module.exports === "object") {
-  module.exports = Object.assign(module.exports.default, module.exports);  // tslint:disable-line: no-unsafe-any
-}
